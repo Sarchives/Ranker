75256250d06a69ddad64703b5956ad0856d09795
SapphireDisD <aliciahuber@outlook.com.ar>
2021-09-26 10:29:14 -0300

    Changed from ASP.NET Core project to Console Project

--- Ranker/Program.cs
+++ Ranker/Program.cs
@@ -1,30 +1,43 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using DSharpPlus;
+using DSharpPlus.SlashCommands;
+using Newtonsoft.Json;
+using System;
 using System.IO;
-using System.Linq;
 using System.Reflection;
 using System.Threading.Tasks;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
-using Newtonsoft.Json;
-using SQLite;
 
 namespace Ranker
 {
     public class Program
     {
-        public static void Main(string[] args)
+        static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            MainAsync().GetAwaiter().GetResult();
         }
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.UseStartup<Startup>();
-                });
+        static async Task MainAsync()
+        {
+            ConfigJson configJson = JsonConvert.DeserializeObject<ConfigJson>(File.ReadAllText("config.json"));
+            SQLiteDatabase database = new SQLiteDatabase(Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), "Ranker.db"));
+
+            DiscordConfiguration configuration = new()
+            {
+                // We may drop JSON token.
+                Token = Environment.GetEnvironmentVariable("RANKER_TOKEN") ?? configJson.Token,
+                TokenType = TokenType.Bot,
+                Intents = DiscordIntents.GuildMembers | DiscordIntents.GuildMessages | DiscordIntents.Guilds
+            };
+
+            DiscordClient client = new(configuration);
+
+            client.AddExtension(new MessageEvent(database));
+
+            var slashCommands = client.UseSlashCommands();
+
+            slashCommands.RegisterCommands<Commands>(configJson.GuildId);
+
+            await client.ConnectAsync();
+            await Task.Delay(-1);
+        }
     }
 }
