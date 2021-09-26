d062b04e5c60a33d904a4a59f96c867eeb353ed6
SapphireDisD <aliciahuber@outlook.com.ar>
2021-09-26 05:54:39 -0300

    Changed stuff

--- Ranker/Controllers/LeaderboardController.cs
+++ Ranker/Controllers/LeaderboardController.cs
@@ -4,18 +4,10 @@ namespace Ranker.Controllers
 {
     public class LeaderboardController : Controller
     {
-        private readonly IDatabase _database;
-
-        public LeaderboardController(IDatabase database)
-        {
-            _database = database;
-        }
-
         [HttpGet("leaderboard/{id}")]
         public IActionResult Index(string id)
         {
             ViewBag.id = id;
-            ViewBag.database = _database;
             return View();
         }
     }
