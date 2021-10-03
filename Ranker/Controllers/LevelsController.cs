2c2e2847408a45a6ebd8d13448e16b6902fe8758
Dongle <programcake@gmail.com>
2021-10-03 21:41:48 +0700

    Add API code

---
+++ Ranker/Controllers/LevelsController.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+
+namespace Ranker
+{
+    [Route("[controller]")]
+    public class LevelsController : Controller
+    {
+        private readonly IDatabase _database;
+
+        public LevelsController(IDatabase database)
+        {
+            _database = database;
+        }
+
+        [Route("{id}")]
+        [HttpGet]
+        public async Task<IActionResult> Get(ulong id, [FromQuery]int page = 0)
+        {
+            try
+            {
+                using (HttpClient client = new())
+                {
+                    client.DefaultRequestHeaders.Authorization =
+                        new AuthenticationHeaderValue(
+                            "Bot",
+                            Environment.GetEnvironmentVariable("RANKER_TOKEN"));
+                    var response = await client.GetAsync($"https://discord.com/api/v9/guilds/{id}");
+                    string responseJson = await response.Content.ReadAsStringAsync();
+                    JToken jsonParsed = JToken.Parse(responseJson);
+                    IEnumerable<Rank> ranks = (await _database.GetAsync())
+                        .Where(f => f.Guild == id)
+                        .OrderByDescending(f => f.Xp)
+                        .Skip(page * 100)
+                        .Take(100);
+
+                    return Ok(new Dictionary<string, object>()
+                    {
+                        {
+                            "guild",
+                            new Dictionary<string, object>()
+                            {
+                                { "name", jsonParsed["name"].Value<string>() },
+                                { "description", jsonParsed["description"].Value<string>() },
+                                { "icon", jsonParsed["icon"].Value<string>() },
+                                { "is_joinable", jsonParsed["rules_channel_id"] != null }
+                            }
+                        },
+                        { "players", ranks }
+                    });
+                }
+            }
+            catch
+            {
+                return BadRequest();
+            }
+        }
+    }
+}
--- Ranker/Program.cs
+++ Ranker/Program.cs
@@ -1,17 +1,5 @@
-﻿using DSharpPlus;
-using DSharpPlus.CommandsNext;
-using DSharpPlus.Entities;
-using DSharpPlus.SlashCommands;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
-using Newtonsoft.Json;
-using System;
-using System.Diagnostics;
-using System.IO;
-using System.Linq;
-using System.Reflection;
-using System.Runtime.InteropServices;
-using System.Threading.Tasks;
+﻿using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
 
 namespace Ranker
 {
@@ -19,83 +7,15 @@ namespace Ranker
     {
         static void Main(string[] args)
         {
-            MainAsync().GetAwaiter().GetResult();
+            BuildWebHost(args);
         }
 
-        static async Task MainAsync()
+        static void BuildWebHost(string[] args)
         {
-            string folder = "";
-
-            if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
-                folder = Path.Combine(
-                    Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData),
-                    "Ranker");
-            else
-                folder = Path.Combine(
-                    Environment.GetFolderPath(Environment.SpecialFolder.UserProfile),
-                    ".ranker");
-
-            if (!Directory.Exists(folder))
-            {
-                Directory.CreateDirectory(folder);
-            }
-
-            ConfigJson configJson = JsonConvert.DeserializeObject<ConfigJson>(File.ReadAllText("config.json"));
-            IDatabase database = new SQLiteDatabase(Path.Combine(folder, "Ranker.db"));
-
-            DiscordConfiguration configuration = new()
-            {
-                Token = Environment.GetEnvironmentVariable("RANKER_TOKEN"),
-                TokenType = TokenType.Bot,
-                Intents = DiscordIntents.GuildMembers | DiscordIntents.GuildMessages | DiscordIntents.Guilds
-            };
-
-            DiscordClient client = new(configuration);
-
-            client.AddExtension(new MessageEvent(database));
-
-            ServiceCollection servCollection = new();
-            servCollection.AddSingleton(database);
-
-            var slashCommands = client.UseSlashCommands(new()
-            {
-                Services = servCollection.BuildServiceProvider()
-            });
-
-            slashCommands.SlashCommandErrored += async (s, e) =>
-            {
-                s.Client.Logger.LogError(e.Exception.ToString());
-
-                bool botOwner = s.Client.CurrentApplication.Owners.Any(x => x.Id == e.Context.User.Id);
-
-                DiscordEmbedBuilder embed = new DiscordEmbedBuilder()
-                    .WithTitle("Error")
-                    .WithColor(DiscordColor.Red)
-                    .WithDescription(botOwner ? $"```{e.Exception}```" : "Please contact the bot owner to get more information.");
-
-                await e.Context.EditResponseAsync(new DiscordWebhookBuilder()
-                    .WithContent("Something went wrong!")
-                    .AddEmbed(embed));
-            };
-
-            slashCommands.RegisterCommands<SlashCommands>(configJson.GuildId);
-
-            var commands = client.UseCommandsNext(new CommandsNextConfiguration()
-            {
-                StringPrefixes = configJson.Prefixes,
-                Services = servCollection.BuildServiceProvider()
-            });
-
-            commands.RegisterCommands<NormalCommands>();
-
-            client.Ready += (s, e) =>
-            {
-                s.Logger.LogInformation("Bot is ready!");
-                return Task.CompletedTask;
-            };
-
-            await client.ConnectAsync();
-            await Task.Delay(-1);
+            WebHost.CreateDefaultBuilder(args)
+                .UseStartup<Startup>()
+                .Build()
+                .Run();
         }
     }
 }
