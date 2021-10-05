d2085756878a9967a95de4e522d3c2b7de6f9840
SapphireDisD <aliciahuber@outlook.com.ar>
2021-10-04 21:16:25 -0300

    New stuff

---
+++ Ranker/Controllers/CommonsController.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ranker
+{
+    [Route("[controller]")]
+    public class CommonsController : Controller
+    {
+
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            try
+            {
+                using (HttpClient client = new())
+                {
+                    client.DefaultRequestHeaders.Authorization =
+                        new AuthenticationHeaderValue(
+                            "Bearer",
+                            Request.Headers["Code"]);
+                    var response = await client.GetAsync("https://discord.com/api/v9/users/@me/guilds");
+                    string responseJson = await response.Content.ReadAsStringAsync();
+                    JArray jsonParsed = JArray.Parse(responseJson);
+
+                    using (HttpClient client2 = new())
+                    {
+                        client2.DefaultRequestHeaders.Authorization =
+                            new AuthenticationHeaderValue(
+                                "Bot",
+                                Environment.GetEnvironmentVariable("RANKER_TOKEN"));
+                        response = await client2.GetAsync("https://discord.com/api/v9/users/@me/guilds");
+                        responseJson = await response.Content.ReadAsStringAsync();
+                        JArray jsonParsed2 = JArray.Parse(responseJson);
+
+                        JArray ready = jsonParsed;
+
+                        jsonParsed.ToList().Select(x => x["id"].Value<string>()).ToList().Except(jsonParsed2.ToList().Select(x => x["id"].Value<string>()).ToList()).ToList().ForEach(nony => {
+                            int index = ready.ToList().FindIndex(x => x["id"].Value<string>() == nony);
+                            if (index > -1)
+                            {
+                                ready.RemoveAt(index);
+                            }
+                        });
+
+                        return Ok(JsonConvert.SerializeObject(ready));
+                    }
+
+                }
+            }
+            catch
+            {
+                return StatusCode(500);
+            }
+        }
+    }
+}
---
+++ Ranker/Controllers/TokenController.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ranker
+{
+    [Route("[controller]")]
+    public class TokenController : Controller
+    {
+
+        ConfigJson configJson;
+
+        public TokenController(ConfigJson config)
+        {
+            configJson = config;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            try
+            {
+                using (HttpClient client = new())
+                {
+                    Dictionary<string, string> dictionary = new Dictionary<string, string>() {
+                        { "client_id", configJson.Client.Id },
+                        { "client_secret", configJson.Client.Secret },
+                        { "grant_type", "authorization_code" },
+                        { "code", Request.Headers["Code"] },
+                        { "redirect_uri", "http://localhost:8000/callback" },
+                        { "scope", "identify guilds" }
+                    };
+
+                    var response = await client.PostAsync($"https://discord.com/api/v9/oauth2/token", new FormUrlEncodedContent(dictionary));
+                    string responseJson = await response.Content.ReadAsStringAsync();
+                    return Ok(responseJson);
+                }
+            }
+            catch
+            {
+                return BadRequest();
+            }
+        }
+    }
+}
