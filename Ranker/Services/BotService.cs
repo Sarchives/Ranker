805f5f00d11bd989e8561c1763343f6e4a8218c3
SimpleBear <itswin11@outlook.com>
2022-09-30 16:33:09 +0200

    debug step 2

--- Ranker/Services/BotService.cs
+++ Ranker/Services/BotService.cs
@@ -56,9 +56,16 @@ namespace Ranker
 
             slashCommands.RegisterCommands<SlashCommands>(localGuildId != null ? Convert.ToUInt64(localGuildId) : null);
 
+            var prefixes = new List<string>();
+
+            prefixes.Add(Environment.GetEnvironmentVariable("PREFIX"));
+
+            Console.WriteLine(Environment.GetEnvironmentVariable("PREFIX"));
+
             var commands = client.UseCommandsNext(new CommandsNextConfiguration()
             {
-                StringPrefixes = new List<string>() { Environment.GetEnvironmentVariable("PREFIX") },
+                StringPrefixes = prefixes,
+                EnableMentionPrefix = true,
                 Services = servCollection.BuildServiceProvider()
             });
 
